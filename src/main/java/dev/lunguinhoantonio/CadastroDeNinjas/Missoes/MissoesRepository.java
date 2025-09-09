933b41556320fef455f23664895cf651f9b396a7
lunguinhoantonio <antoniolunguinho007@gmail.com>
2025-09-09 00:29:17 -0300

    feat: add MissoesRepository Interface

---
+++ src/main/java/dev/lunguinhoantonio/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package dev.lunguinhoantonio.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
