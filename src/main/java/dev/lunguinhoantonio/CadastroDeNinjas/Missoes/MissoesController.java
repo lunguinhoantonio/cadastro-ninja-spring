17a0066ba572a04618de0b6e10004d9bd34729ca
lunguinhoantonio <antoniolunguinho007@gmail.com>
2025-09-14 04:20:21 -0300

    feat: add MissoesController with mock

---
+++ src/main/java/dev/lunguinhoantonio/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,29 @@
+package dev.lunguinhoantonio.CadastroDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Missões listadas com sucesso!";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão criada com sucesso!";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada com sucesso!";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletada com sucesso!";
+    }
+
+}
