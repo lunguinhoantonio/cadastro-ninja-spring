5892baf84b78ca25aa267ea8d6291b0d63144602
lunguinhoantonio <antoniolunguinho007@gmail.com>
2025-09-14 04:05:50 -0300

    feat: update controller with mock data

--- src/main/java/dev/lunguinhoantonio/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/lunguinhoantonio/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,15 +1,38 @@
 package dev.lunguinhoantonio.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
 
-    @GetMapping("/")
+    @GetMapping("/boasvindas")
     public String welcome() {
         return "<span style='color: blue; background-color: black;'><strong>Olá Mundo!</strong></span>";
+    }
+
+    @PostMapping("/adicionar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar ninjas";
+    }
+
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId() {
+        return "Mostrar ninja por ID";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarNinjaPorId() {
+        return "Alterar ninja por ID";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Ninja deletado por ID";
     }
 }
