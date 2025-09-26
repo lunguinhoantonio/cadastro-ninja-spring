f52e925ad31de75c9a4104322b27e1ce9fd1af51
lunguinhoantonio <antoniolunguinho007@gmail.com>
2025-09-26 02:30:04 -0300

    chore: add one more security layer with DTO and mapping manually with Mapper

--- src/main/java/dev/lunguinhoantonio/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/lunguinhoantonio/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,18 @@
 package dev.lunguinhoantonio.CadastroDeNinjas.Ninjas;
+import dev.lunguinhoantonio.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private int idade;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private String rank;
+    private MissoesModel missoes;
 }
