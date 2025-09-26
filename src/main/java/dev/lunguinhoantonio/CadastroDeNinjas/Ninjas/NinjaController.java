35d491c904630353f71e3e3aef6723fbb5eb9c0f
lunguinhoantonio <antoniolunguinho007@gmail.com>
2025-09-26 04:16:27 -0300

    fix: takes the responsibility from my Model and passes it to my DTO using Streams in Mapper

--- src/main/java/dev/lunguinhoantonio/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/lunguinhoantonio/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -25,17 +25,17 @@ public class NinjaController {
     }
 
     @GetMapping("/listar")
-    public List<NinjaModel> listarNinjas() {
+    public List<NinjaDTO> listarNinjas() {
         return ninjaService.listarNinjas();
     }
 
     @GetMapping("/listar/{id}")
-    public NinjaModel listarNinjasPorId(@PathVariable Long id) {
+    public NinjaDTO listarNinjasPorId(@PathVariable Long id) {
         return ninjaService.listarNinjasPorID(id);
     }
 
     @PutMapping("/alterar/{id}")
-    public NinjaModel alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaModel ninjaAtualizado) {
+    public NinjaDTO alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado) {
         return ninjaService.atualizarNinja(id, ninjaAtualizado);
     }
 
